44371640d9ca9e090e60598d183352ea22407bd2
mokronos <sebastian@hirts.de>
2023-01-21 13:44:06 +0100

    adjusted stuff to test react basics

--- _content/splitter.md
+++ _content/splitter.md
@@ -5,4 +5,16 @@ layout: special
 
 # Splittermond
 
+<!-- create react div for some information -->
+<div id="root"></div>
+<div id="imgelement"></div>
 
+<!-- <script> -->
+<!--     alert("Hello World"); -->
+<!-- </script> -->
+<!-- load react -->
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+<!-- Load rest of js -->
+<script type="text/babel" src="/random_stuff/oneshot/app.js"></script>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,18 @@
 // create a message that displays to check if the script is working on load
+// alert('Hello World');
+// console.log('main.js loaded');
+
 alert('Hello World');
-console.log('main.js loaded');
+// get root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// create list of variables 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const element = <h1>Hello, world!</h1>;
 
-// function to make the background a little whiter for half a second with a fade
-
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+// let val = "test";
+// val += numbers[0];
+// root.render(element);
+// val += element;
+// root.render(val);
+// root.render(numbers[0]);
+root.render(element);
