c053221f11387e3fbbacf1e1b045346d3b8b4167
mokronos <sebastian@hirts.de>
2023-01-23 19:40:21 +0100

    basic grid layout, random selection of box

--- dev-oneshot/src/App.js
+++ dev-oneshot/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import jsondata from './data.json';
+
+class Image extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imgPath = process.env.PUBLIC_URL + '/images/' + this.props.url;
+    }
+    render() {
+        return <img src={this.imgPath} />;
+    }
+}
+class Box extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="box">
+                <h1> {this.props.item.name} </h1>
+                <Image url={this.props.item.img_src} />
+                <p> {this.props.item.description} </p>
+            </div>
+        );
+    }
+}
+
+class Container extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {curr_data: this.props.data};
+        this.getRandomEnemy = this.getRandomEnemy.bind(this);
+        this.seeAll = this.seeAll.bind(this);
+    };
+    getRandomEnemy() {
+        console.log("get random enemy");
+        this.setState({curr_data: [this.props.data[Math.floor(Math.random() * this.props.data.length)]]});
+    }
+    seeAll() {
+        console.log("see all");
+        this.setState({curr_data: this.props.data});
+    }
+    render() {
+        const cards = this.state.curr_data.map((item) => 
+            <Box key={item.id} item={item} />
+        );
+        return (
+            <div className="wrapper">
+            <button onClick={this.getRandomEnemy}> get random enemy </button>
+            <button onClick={this.seeAll}> see all </button>
+            {cards}
+            </div>)
+        ;
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          EEEEEDIT <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+        <Container data={jsondata}/>
+        </div>
+    );
 }
 
 export default App;
