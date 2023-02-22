98195e823b1232745935cb7349ce7dea909c667f
mokronos <sebastian@hirts.de>
2023-02-22 01:22:39 +0100

    Add folder for dicolab

---
+++ random_stuff/dicolab/home1/matlab/main.m
@@ -0,0 +1,26 @@
+image_dir = '../images';
+% Define the signal constellation
+labels = {'00','01','11','10'};
+coords = [-1-1i,-1+1i , 1+1i , 1-1i];
+
+% plot
+scatter(real(coords), imag(coords));
+grid on;
+
+% add labels
+for i = 1:length(coords)
+    text(real(coords(i))+0.1, imag(coords(i))+0.1, labels(i), 'FontSize', 12);
+end
+
+% Set the plot limits and axis labels
+xlim([-2 2]);
+ylim([-2 2]);
+xlabel('In-Phase');
+ylabel('Quadrature');
+
+% Set the plot title
+title('4-QAM Signal Constellation');
+
+% Save the plot as a PNG file
+saveas(gcf, fullfile(image_dir, '4_QAM.png'));
+close;
